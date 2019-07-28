ec19959631222c364135c54e8988a14517408e4d
Deexit1 <deexitpatel4@gmail.com>
2019-07-28 18:51:18 +0530

    start

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Deexit", age: 19},
+      { name: "Smit", age: 20},
+      { name: "Viraj", age: 19}
+    ]
+  }
+  switchHandler = () => {
+    console.log("Clicked");
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> I'm react developer </h1>
+        <button onClick = {this.switchHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbie: Driving</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
       </div>
+      // React.createElement('div',{className: 'App'}, React.createElement('h1',null,"I'm a react developer."))
     );
   }
 }
